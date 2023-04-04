280b9e731a98206ede2375853d1d7cb055a7c702
Kim Robert <kimrobert.nb@gmail.com>
2023-04-04 11:25:30 +0200

    c07 add image model

---
+++ rm-back/data/models/Image.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const imageSchema = new Schema({
+  fileName: {
+    type: String
+  },
+  originalName: {
+    type: String
+  },
+  mimeType: {
+    type: String
+  },
+  url: {
+    type: String
+  },
+  path: {
+    type: String
+  },
+  size: {
+    type: Number
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.models.Image || mongoose.model('Image', imageSchema)

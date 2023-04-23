c6be782a9cbaf2490528bdf959169d9b4ae4127c
Kim Robert <kimrobert.nb@gmail.com>
2023-04-23 16:16:06 +0200

    fc05 wip register service bug cors not allowed

--- rm-front/src/pages/user/User.js
+++ rm-front/src/pages/user/User.js
@@ -1,21 +1,31 @@
 // IMPORTS
 // Modules
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 // Components
 import LoginForm from '../../components/login-form/LoginForm'
 import RegisterForm from '../../components/register-form/RegisterForm'
-
 // Styles
 import './User.scss'
 
 function User () {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [isRegistered, setIsRegistered] = useState(true)
   // const [profile, setProfile] = useState(null)
 
+  // toggle form
   const toggleForm = () => {
     return setIsRegistered(!isRegistered)
   }
+  // check if user is logged in and return user data
+  // const [isLoggedIn, setIsLoggedIn] = useState(false)
+  // const { dispatch, state: { error, user, loading } } = useAuth()
+  // useEffect(() => {
+  //   if (user) {
+  //     setIsLoggedIn(true)
+  //   } else {
+  //     setIsLoggedIn(false)
+  //   }
+  // }, [user])
+  // Rendering
   return (
     <section className='section __user'>
       {isRegistered
